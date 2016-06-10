fec4b4a4583998016ae138e42117347f15215bce
jack <jack@jackdemacbook-air.local>
2016-06-11 01:24:25 +0800

    Initial Commit

---
+++ frist OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  frist OC
+//
+//  Created by jack on 16/6/11.
+//  Copyright © 2016年 jack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
