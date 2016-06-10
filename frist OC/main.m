dd39be124e2fa8277a35eda96693716a3820c562
jack <jack@jackdemacbook-air.local>
2016-06-11 01:59:39 +0800

    1.first release function/function.h for Objective-C Language. 2.add Calculation code

--- frist OC/main.m
+++ frist OC/main.m
@@ -6,40 +6,47 @@
 //  Copyright © 2016年 jack. All rights reserved.
 //
 
-//#import <Foundation/Foundation.h>
-//
-//int main(int argc, const char * argv[]) {
-//    @autoreleasepool {
-//        // insert code here...
-//        
-//        
-//        NSLog(@"Hello, World!");
-//    }
-//    return 0;
-//}
+#import <Foundation/Foundation.h>
 
-#import<Foundation/Foundation.h>
-float circlearea(float theradius);
-float rectanglearea(float width,float height);
-int main()
-{
-    float picturewidth, pictureheight,picturesurfacearea, circleradius, circlesurfacearea;
-    picturewidth=8.0;
-    pictureheight=4.5;
-    circleradius=5.0;
-    picturesurfacearea=rectanglearea(picturewidth,pictureheight);
-    circlesurfacearea=circlearea(circleradius);
-    NSLog(@"area of circle: %10.2f.",circlesurfacearea);
-    NSLog(@"area of picture: %f. ",picturesurfacearea);
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        float celsius, tempInFahrenheit;
+        for (tempInFahrenheit = 0; tempInFahrenheit <= 200; tempInFahrenheit
+             =tempInFahrenheit + 20) {
+            celsius = (tempInFahrenheit - 32.0) * 5.0 / 9.0;
+            NSLog(@"%10.2f -> %10.2f", tempInFahrenheit, celsius); }
+        
+        
+        NSLog(@"Hello, World!");
+    }
     return 0;
 }
-float circlearea(float theradius)//first custom function
-{
-    float thearea;
-    thearea=3.1416*theradius*theradius;
-    return thearea;
-}
-float rectanglearea(float width,float height)//second custom function
-{
-    return width*height;
-}
+//#import<Foundation/Foundation.h>
+//float circlearea(float theradius);
+//float rectanglearea(float width,float height);
+//int main()
+//{
+//    float picturewidth, pictureheight,picturesurfacearea, circleradius, circlesurfacearea;
+//    picturewidth=8.0;
+//    pictureheight=4.5;
+//    circleradius=5.0;
+//    picturesurfacearea=rectanglearea(picturewidth,pictureheight);
+//    circlesurfacearea=circlearea(circleradius);
+//    NSLog(@"area of circle: %10.2f.",circlesurfacearea);
+//    NSLog(@"area of picture: %f. ",picturesurfacearea);
+//    return 0;
+//}
+//float circlearea(float theradius)//first custom function
+//{
+//    float thearea;
+//    thearea=3.1416*theradius*theradius;
+//    return thearea;
+//}
+//float rectanglearea(float width,float height)//second custom function
+//{
+//    return width*height;
+//}
+
+
+
